523933ec4cf24a74eeb81ee4d87376187f255d09
Bhhorness <Beth.Horness@noaa.gov>
2016-01-07 10:03:13 -0800

    Update MultiSpecies.sql
    -- 1/6/2016  Script was found to be joining using common name only instead of scientific name.  Also, the weights were being set to zero based
    --           on if the common name was present.  This was a serious error since many species table entries have no common name.  Updated 
    --           the join to test on both the scientific and common names [nvl(nw_sciname, nw_commonname)] because a few of the species table 
    --           entries have no scientific names. Updated the weights fields to set to zero when an aggregate_catch record for the species of interest
    --           is absent.  BHH

--- trawl/MultiSpecies.sql
+++ trawl/MultiSpecies.sql
@@ -1,5 +1,10 @@
 -- Multi-species : Satisfactory Only
 -- 9/1/2014
+-- 1/6/2016  Script was found to be joining using common name only instead of scientific name.  Also, the weights were being set to zero based
+--           on if the common name was present.  This was a serious error since many species table entries have no common name.  Updated 
+--           the join to test on both the scientific and common names [nvl(nw_sciname, nw_commonname)] because a few of the species table 
+--           entries have no scientific names. Updated the weights fields to set to zero when an aggregate_catch record for the species of interest
+--           is absent.  BHH
 select *
 from (
 select
@@ -16,15 +21,15 @@ select
        h.best_depth_type,
        h.DURATION_START2END_HR,
        h.area_swept_ha,
-       decode(c.species, null, 0, c.haul_wt_kg) as haul_wt_kg,
-       decode(c.species, null, 0, c.avg_wt_kg) as avg_wt_kg,
+       decode(c.agg_catch_id, null, 0, c.haul_wt_kg) as haul_wt_kg,
+       decode(c.agg_catch_id, null, 0, c.avg_wt_kg) as avg_wt_kg,
        h.Operation_Status
 from master.VW_Assessment_Species aspp
 inner join master.VW_Haul_Chars_Sat h
 on aspp.scientific_name in(&sp)   -- Must contain the same species as listed in the Pivot In clause below (without the aliases)
 left join master.VW_Catch_Wts_Nums_All c
 on c.sample_id = h.sample_id
-and c.species = aspp.species
+and nvl(c.scientific_name, c.species) = nvl(aspp.scientific_name, aspp.species)
 where h.project_cycle between 'Cycle &beginyr' and 'Cycle &endyr'    -- Can be used to limit the survey year range (or can be left out entirely).
 )
 PIVOT (
